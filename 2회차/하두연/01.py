ad7bc42a7276924b291c46f26a767cdedffc7dd2
hady1024 <hady1024@gmail.com>
2022-07-03 17:09:10 +0900

    개인 프로젝트

--- 2회차/하두연/01.py
+++ 2회차/하두연/01.py
@@ -5,9 +5,10 @@ with open('./data/fruits.txt', 'r', encoding='utf-8') as f:
 
     for line in  lines:
         count += 1
+
+    with open('01.txt', 'w', encoding='utf-8') as a:
+        a.write(f'{count}')
     
-with open('01.txt', 'w', encoding='utf-8') as a:
-    a.write(f'{count}')
 
 
 
