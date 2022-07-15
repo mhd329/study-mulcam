1aa7d348baf273f1225465d02f777cd4a18ddebb
nahkim <nahkim@student.42seoul.kr>
2022-07-15 16:43:09 +0900

    3회차_김나현_06 완료

--- 3회차/김나현/06.py
+++ 3회차/김나현/06.py
@@ -1,10 +1,34 @@
 import json
+from operator import itemgetter
 from pprint import pprint
 
+def search_genre_name(movie, genres):
+    genres_names = []
+    genre_id = movie.get('genre_ids')
+    for id in genre_id:
+        for j in range(len(genres)):
+            if (id == genres[j].get('id')):
+                genres_names.append(genres[j].get('name'))
+                break
+    return (genres_names)
 
 def movie_info(movies, genres):
     pass 
-    # 여기에 코드를 작성합니다.  
+
+    res = []
+    movies_list = sorted(movies, key=itemgetter('vote_average'), reverse=True)
+
+    for movie in movies_list:
+        search_name = search_genre_name(movie, genres)
+
+        res.append({
+            'genre_names': search_name,
+            'id': movie.get('id'),
+            'overview': movie.get('overview'),
+            'title': movie.get('title'),
+            'vote_average': movie.get('vote_average')
+    })
+    return (res)
         
         
 # 아래의 코드는 수정하지 않습니다.
