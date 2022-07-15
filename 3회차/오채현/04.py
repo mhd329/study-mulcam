809d4e24dbfc937cc7fb26b54753e57352c30751
chaehyun_oh <ohchaehh@gmail.com>
2022-07-15 15:46:39 +0900

    04 풀이

--- 3회차/오채현/04.py
+++ 3회차/오채현/04.py
@@ -5,6 +5,17 @@ from pprint import pprint
 def movie_info(movie):
     pass 
     # 여기에 코드를 작성합니다.    
+    result = {
+        'genre_ids': movie.get('genre_ids'),
+        'id': movie.get('id'),
+        'overview': movie.get('overview'),
+        'title': movie.get('title'),
+        'vote_average': movie.get('vote_average')
+    }
+        
+    return result
+
+
 
 
 # 아래의 코드는 수정하지 않습니다.
