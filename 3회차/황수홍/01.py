f41158cda9abb6085945a0f1da4334206b8e7b50
hong2HSH <tnghd1717@gmail.com>
2022-07-15 14:26:09 +0900

    01.py 수정본

--- 3회차/황수홍/01.py
+++ 3회차/황수홍/01.py
@@ -1,5 +1,9 @@
 with open('./data/fruits.txt', 'r', encoding='utf-8')as f:
-    text = f.read()
-a = str(text.count('\n')+1)
-with open('01.txt', 'w', encoding='utf-8')as f1:
-    f1.write(a)
+    lines = f.readline()
+    cnt = 0
+    for line in f:
+        if lines == ' ':
+            break
+        cnt += 1
+    with open('01.txt', 'w', encoding='utf-8')as f1:
+        f1.write(str(cnt+1))
---
+++ 3회차/황수홍/02.py
@@ -0,0 +1,7 @@
+# with open('./data/fruits.txt', 'r', encoding='utf-8')as f:
+#     cnt = 0
+#     fruit = f.read().split()
+#     for i in fruit:
+#         if i.startswith('berry'):
+#             cnt += 1
+#     print(cnt)
