9943773ebd84a1989d80bb20ede12d52684139fd
nahkim <nahkim@student.42seoul.kr>
2022-07-15 16:42:48 +0900

    3회차_김나현_04 완료

--- 3회차/김나현/04.py
+++ 3회차/김나현/04.py
@@ -4,7 +4,13 @@ from pprint import pprint
 
 def movie_info(movie):
     pass 
-    # 여기에 코드를 작성합니다.    
+    return {
+    'genre_ids': movie.get('genre_ids'),
+    'id': movie.get('id'),
+    'overview': movie.get('overview'),
+    'title': movie.get('title'),
+    'vote_average': movie.get('vote_average')
+    }
 
 
 # 아래의 코드는 수정하지 않습니다.
