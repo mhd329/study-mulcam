51a202740a2f38dc8143e858255273648dc8112b
jincde <asdzxc@kakao.com>
2022-07-15 19:50:10 +0900

    05.py

--- 2회차/장영진/05.py
+++ 2회차/장영진/05.py
@@ -3,9 +3,24 @@ from pprint import pprint
 
 
 def movie_info(movie, genres):
-    pass 
-    # 여기에 코드를 작성합니다.  
-        
+    info = {
+        'id': movie.get('id'),
+        'title': movie.get('title'),
+        'vote_average': movie.get('vote_average'),
+        'overview': movie.get('overview')
+    }
+
+    n = [] # 최종값을 저장할 리스트
+    for i in movie['genre_ids']: # genre_ids가 18, 80 i는 18과 80을 순회
+        for j in genres: # genres의 id를 순회
+            if j['id'] == i: # j가 i의 값과 같다면 최종 리스트에 name을 추가
+                n.append(j['name'])
+
+    info['genres_names'] = n
+
+    return info
+    # 여기에 코드를 작성합니다.
+
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
