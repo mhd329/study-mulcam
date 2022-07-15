9233029bf3a692d97254615743d05ac2bad2e828
Bonbon315 <n202u123@gmail.com>
2022-07-15 14:22:05 +0900

    read write 예제 완료

---
+++ 2회차/조본희/03.py
@@ -0,0 +1,20 @@
+from pprint import pprint
+
+
+with open('data/fruits.txt', 'r', encoding='utf-8') as r:
+    text = r.read()
+    fruit = text.split('\n')
+
+#Dictionary 생성해서 key, value 저장
+fruits = list(fruit)
+fruitDict = {}
+
+for i in fruits:
+    fruitDict[i] = 0
+for i in fruits:
+    fruitDict[i] += 1
+
+#txt파일 만들어서 저장
+with open('03.txt', 'w', encoding='utf-8') as w:
+    for k, v in fruitDict.items():
+        w.write(f'{k} {v}\n')
