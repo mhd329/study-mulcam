2c3cb65081ce99e9ac1a905d31d8122b48f4526c
myeonghwan57 <myeonghwan57@gmail.com>
2022-07-15 17:41:01 +0900

    프로젝트 실습 풀이

--- 3회차/김명환/05.py
+++ 3회차/김명환/05.py
@@ -1,11 +1,31 @@
 import json
 from pprint import pprint
+from re import M
 
 
 def movie_info(movie, genres):
-    print(movie,genres)
-    # 여기에 코드를 작성합니다.  
-        
+    f1= open('data\\movie.json','r',encoding = 'utf-8')
+    movie =  json.load(f1)
+    genre_names=list()
+    f2= open('data\\genres.json','r',encoding = 'utf-8')
+    genres =  json.load(f2)
+    
+    for i in range(len((movie['genre_ids']))): #2 0,1
+        for j in range(len(genres)):
+            if movie['genre_ids'][i] == genres[j]['id']:
+                genre_names.append(genres[j]['name'])
+    movie['genre_names']=genre_names
+   
+
+    result ={
+        'genre_names' : movie.get('genre_names'),
+        'genre_ids' : movie.get('genre_id'),
+        'id' : movie.get('id'),
+        'overview' : movie.get('overview'),
+        'title' : movie.get('title'),
+        'vote_average' : movie.get('vote_average')
+    }
+    pprint (result)        
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
--- 3회차/김명환/06.py
+++ 3회차/김명환/06.py
@@ -3,9 +3,28 @@ from pprint import pprint
 
 
 def movie_info(movies, genres):
-    pass 
-    # 여기에 코드를 작성합니다.  
-        
+    f1= open('data\\movies.json','r',encoding = 'utf-8')
+    movies =  json.load(f1)
+    genre_names=list()
+    f2= open('data\\genres.json','r',encoding = 'utf-8')
+    genres =  json.load(f2)
+     
+    for movie in movies: 
+        for i in range(len((movie['genre_ids']))): #2 0,1
+            for j in range(len(genres)):
+                if movie['genre_ids'][i] == genres[j]['id']:
+                    genre_names.append(genres[j]['name'])
+        movie['genre_names'] = genre_names
+   
+    result = {
+        'genre_names' : movies.get('genre_names'),
+        'genre_ids' : movies.get('genre_id'),
+        'id' : movies.get('id'),
+        'overview' : movies.get('overview'),
+        'title' : movies.get('title'),
+        'vote_average' : movies.get('vote_average')
+    }
+    pprint (result)            
         
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
