394976087bfc3df97c95ee034af28e1c87b8dfc6
mgpo-98 <csi4613@naver.com>
2022-07-15 17:17:08 +0900

    0715문제풀이

--- 2회차/조승윤/05.py
+++ 2회차/조승윤/05.py
@@ -1,11 +1,21 @@
 import json
 from pprint import pprint
+from re import A
 
 
 def movie_info(movie, genres):
     pass 
     # 여기에 코드를 작성합니다.  
-        
+    result = []
+    ele ={}
+    key= [id, genre_names, overview, title ,vote_average]
+
+    for movies in movie :
+        for k in key:
+            ele[k] = movies.get(k)
+            result.append(ele)
+    return result
+pprint(result)
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
