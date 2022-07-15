90e2e53c053702193b6b8dd11d73b67111b6bb38
nahkim <nahkim@student.42seoul.kr>
2022-07-15 16:42:58 +0900

    3회차_김나현_05 완료

--- 3회차/김나현/05.py
+++ 3회차/김나현/05.py
@@ -4,7 +4,31 @@ from pprint import pprint
 
 def movie_info(movie, genres):
     pass 
-    # 여기에 코드를 작성합니다.  
+    genre_ids = movie.get('genre_ids')
+    # print(genre_ids)
+    res = []
+    # print(genres[0])    # {'id': 28, 'name': 'Action'}
+    # g = genres[0]   # 28
+    # print(g.get('name'))
+
+    # print('len', len(genres))
+
+    for id in genre_ids:
+        # print('id', id)
+        for j in range(len(genres)):
+            # print('id', id)
+            # print('genres[j]', genres[j])
+            if (id == genres[j].get('id')):
+                # print('append', genres[j].get('name'))
+                res.append(genres[j].get('name'))
+                break
+    return {
+        'genre_names': res,
+        'id': movie.get('id'),
+        'overview': movie.get('overview'),
+        'title': movie.get('title'),
+        'vote_average': movie.get('vote_average')
+    }
         
 
 # 아래의 코드는 수정하지 않습니다.
