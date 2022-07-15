cd1a74bfd120f9cb1d868551b1450f2edc39741f
Myeonghak Lee <mhmh779@naver.com>
2022-07-15 17:41:32 +0900

    프로젝트

--- 3회차/이명학/05.py
+++ 3회차/이명학/05.py
@@ -3,9 +3,23 @@ from pprint import pprint
 
 
 def movie_info(movie, genres):
-    pass 
-    # 여기에 코드를 작성합니다.  
-        
+    pass
+    # 여기에 코드를 작성합니다.
+    moive_info_dict = {}
+    g_list = []
+    for i in range(len(genres)):
+        if genres[i].get('id') == 80:
+            g_list.append(genres[i].get('name'))
+        if genres[i].get('id') == 18:
+            g_list.append(genres[i].get('name'))
+    moive_info_dict['genre_names'] = g_list
+    moive_info_dict['id'] = movie["id"]
+    moive_info_dict['overview'] = movie["overview"]
+    moive_info_dict['title'] = movie['title']
+    moive_info_dict['vote_average'] = movie['vote_average']
+
+    return moive_info_dict
+
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
--- 3회차/이명학/06.py
+++ 3회차/이명학/06.py
@@ -3,10 +3,31 @@ from pprint import pprint
 
 
 def movie_info(movies, genres):
-    pass 
-    # 여기에 코드를 작성합니다.  
-        
-        
+    pass
+    # 여기에 코드를 작성합니다.
+    result = []
+    genre_id_name = {}
+
+    for d in genres:
+        genre_id_name[d['id']] = d['name']
+
+    for i in range(len(movies)):
+        genre_name = []
+        for j in movies[i]['genre_ids']:
+            genre_name.append(genre_id_name[j])
+        movies[i]['genre_names'] = genre_name
+
+    for d in movies:
+        temp = {}
+        temp['genre_names'] = d['genre_names']
+        temp['id'] = d['id']
+        temp['overview'] = d['overview']
+        temp['title'] = d['title']
+        temp['vote_average'] = d['vote_average']
+        result.append(temp)
+    return result
+
+
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
     movies_json = open('data/movies.json', encoding='UTF8')
