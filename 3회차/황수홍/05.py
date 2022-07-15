fcec598be63e6bca0e758c059c6688e542dfa34e
hong2HSH <tnghd1717@gmail.com>
2022-07-15 16:59:12 +0900

    05.py

--- 3회차/황수홍/05.py
+++ 3회차/황수홍/05.py
@@ -1,12 +1,24 @@
 import json
 from pprint import pprint
+from this import d
 
 
 def movie_info(movie, genres):
     pass 
     # 여기에 코드를 작성합니다.  
-        
+    result = {
+        'genre_names': [],
+        'id': movie.get('id'),
+        'overview': movie.get('overview'),
+        'title': movie.get('title'),
+        'vote_average': movie.get('vote_average')        
+    }
+    for i in movie.get('genre_ids'):
+        for genre in genres:
+            if genre.get('id') == i:
+                result['genre_names'].append(genre.get('name'))
 
+    return result
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
     movie_json = open('data/movie.json', encoding='UTF8')
