49811e89cea7ede5be9556ea30aef00fc24fd52e
hong2HSH <tnghd1717@gmail.com>
2022-07-15 17:23:29 +0900

    06.py완료

--- 3회차/황수홍/06.py
+++ 3회차/황수홍/06.py
@@ -5,7 +5,22 @@ from pprint import pprint
 def movie_info(movies, genres):
     pass 
     # 여기에 코드를 작성합니다.  
-        
+    result = [] # 어떤영화 ?
+    for movie in movies:
+        movie_info = {
+        'genre_names': [],
+        'id': movie.get('id'),
+        'overview': movie.get('overview'),
+        'title': movie.get('title'),
+        'vote_average': movie.get('vote_average')        
+    }
+        for id in movie.get('genre_ids'):
+            for genre in genres:
+                if genre.get('id') == id:
+                    movie_info['genre_names'].append(genre.get('name'))
+
+        result.append(movie_info)
+    return result
         
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
