c2736c28cea10bafb4dc7fbc7f977a8c9128a4d6
wnsn8546 <wnsn8546@gmail.com>
2022-07-15 16:06:19 +0900

    1회차 최준우 0715 실습 문제 풀이

--- 1회차/최준우/04.py
+++ 1회차/최준우/04.py
@@ -1,11 +1,19 @@
 import json
 from pprint import pprint
+from turtle import title
 
 
 def movie_info(movie):
     pass 
     # 여기에 코드를 작성합니다.    
-
+    my_dict = {
+        'genre_ids': movie['genre_ids'],
+        'id' : movie['id'],
+        'overview' : movie['overview'],
+        'title' : movie['title'],
+        'vote_average' : movie['vote_average']        
+        }
+    return my_dict
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
--- 1회차/최준우/06.py
+++ 1회차/최준우/06.py
@@ -5,7 +5,26 @@ from pprint import pprint
 def movie_info(movies, genres):
     pass 
     # 여기에 코드를 작성합니다.  
-        
+    my_dict_list=[]
+    for movie in movies:
+        names =[]
+        for i in movie['genre_ids']:
+            for j in range(0, len(genres)):
+                if genres[j]['id'] == i:
+                    names.append(genres[j]['name'])
+        my_dict = {}
+        my_dict = {
+            'genre_names': names,
+            'id' : movie['id'],
+            'overview' : movie['overview'],
+            'title' : movie['title'],
+            'vote_average' : movie['vote_average']
+            }
+        my_dict_list.append(my_dict)
+
+    return my_dict_list
+# def names(movie):
+
         
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
