f0548f655567d130ecb2b34c19c3e117cefe66ef
jincde <asdzxc@kakao.com>
2022-07-15 19:20:51 +0900

    04.py 완료

--- 2회차/장영진/04.py
+++ 2회차/장영진/04.py
@@ -3,8 +3,14 @@ from pprint import pprint
 
 
 def movie_info(movie):
-    pass 
-    # 여기에 코드를 작성합니다.    
+    info = {
+        'id' : movie.get('id'),
+        'title' : movie.get('title'),
+        'vote_average' : movie.get('vote_average'),
+        'overview' : movie.get('overview'),
+        'genre_ids' : movie.get('genre_ids')
+    }
+    return info
 
 
 # 아래의 코드는 수정하지 않습니다.
