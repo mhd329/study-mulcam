ecc295427be27df94968881a66d829073ef9c836
jincde <asdzxc@kakao.com>
2022-07-15 15:47:44 +0900

    02.py 완료

--- 2회차/장영진/02.py
+++ 2회차/장영진/02.py
@@ -1,11 +1,19 @@
-with open('data/fruits.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
-    names = text.split()
+with open('data/fruits.txt', 'r', encoding='utf-8') as r:
+    text = r.read()
+    names = list(text.split('\n'))
     cnt = 0
 
-    for name in names:
-        if name.endswith('berry'):
-            berry = (se)
-            cnt += 1
-    
-    print(cnt)
+berry = [] # berry로 끝나는 문자열 저장
+for name in names:
+    if name.endswith('berry'):
+        berry.append(name)
+
+berryList = [] # 중복 삭제된 리스트 저장
+for i in berry:
+    if i not in berryList:
+        berryList.append(i)
+
+print(len(berryList)) # berry로 끝나는 과일 갯수
+
+for fruit in berryList: # berry로 끝나는 과일 목록
+    print(fruit)
