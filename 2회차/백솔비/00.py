97e2906c693f8583c36a7c4f107f77ac3ad00782
hae-on <solbi2004@naver.com>
2022-07-15 16:49:39 +0900

    00 file 수정

--- 2회차/백솔비/00.py
+++ 2회차/백솔비/00.py
@@ -1,5 +1,5 @@
 with open('2회차/백솔비/00.txt', 'w', encoding='utf-8') as f:
-    print("2회차 백솔비")
-    print("Hello, Python!")
+    f.write("2회차 백솔비\n")
+    f.write("Hello, Python!\n")
     for i in range(1, 6):
-        print(f'{i}일차 파이썬 공부 중')
+        f.write(f'{i}일차 파이썬 공부 중\n')
