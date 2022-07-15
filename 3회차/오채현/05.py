f4f9e8e9a31922f4b9906eff7e9f893c807eeab6
chaehyun_oh <ohchaehh@gmail.com>
2022-07-15 16:40:12 +0900

    05 풀이

--- 3회차/오채현/05.py
+++ 3회차/오채현/05.py
@@ -5,7 +5,27 @@ from pprint import pprint
 def movie_info(movie, genres):
     pass 
     # 여기에 코드를 작성합니다.  
-        
+    # print(genres)
+    genresId = movie.get('genre_ids')
+    genresNames = []
+    print(len(genresId))
+    
+    for i in range(len(genres)):
+        for j in range(len(genresId)):
+            if genres[i].get('id') == genresId[j]:
+                genresNames.append(genres[i].get('name'))
+                
+                
+    result = {
+        'genre_names':  genresNames,
+        'id': movie.get('id'),
+        'overview': movie.get('overview'),
+        'title': movie.get('title'),
+        'vote_average': movie.get('vote_average')
+    }
+
+    return result
+
 
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
