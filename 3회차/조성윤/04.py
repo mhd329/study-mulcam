ea336006eb0580d46b4160b6010a4441069bb642
jsy78 <whtjd1541@gmail.com>
2022-07-15 15:37:53 +0900

    add project 01-06

--- 3회차/조성윤/04.py
+++ 3회차/조성윤/04.py
@@ -3,7 +3,14 @@ from pprint import pprint
 
 
 def movie_info(movie):
-    pass 
+    result = {}    
+    result['genre_ids'] = movie['genre_ids']
+    result['id'] = movie['id']
+    result['overview'] = movie['overview']
+    result['title'] = movie['title']
+    result['vote_average'] = movie['vote_average']
+
+    return result
     # 여기에 코드를 작성합니다.    
 
 
@@ -11,5 +18,5 @@ def movie_info(movie):
 if __name__ == '__main__':
     movie_json = open('data/movie.json', encoding='UTF8')
     movie = json.load(movie_json)
-    
+
     pprint(movie_info(movie))
