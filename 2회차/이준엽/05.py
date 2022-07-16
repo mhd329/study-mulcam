84d9eb4c1495c99bbffff4c8583afa7c54b070de
dortkthf <dortkthf@gmail.com>
2022-07-16 23:41:34 +0900

    드디어 수업예제 완료

--- 2회차/이준엽/05.py
+++ 2회차/이준엽/05.py
@@ -4,17 +4,26 @@ from pprint import pprint
 
 def movie_info(movie, genres):
     pass 
-    # 여기에 코드를 작성합니다.  
+# 여기에 코드를 작성합니다.  
+    g_names = []
     movieid = movie.get('genre_ids')
-    movieg=movieid.split()
+    for id in movieid :
+        for a in genres_list:
+            if a['id'] == id :
+                g_names.append(a.get('name'))
     result = {
-        'genrre_names' : genres[movieg],
-        'id' : movie.get('id'),
-        'overvuew' : movie.get('overview'),
-        'title' : movie.get('title'),
-        'vote_average' : movie.get('vote_average')
-    }     
-    return result
+        'genre_names' : g_names,
+        'id': movie.get('id'),
+        'overview':movie.get('overview'),
+        'title':movie.get('title'),
+        'vote_average':movie.get('vote_average')
+    }
+
+
+    test = []
+    test_dic = {'id' : 1,'power' : 2}
+    test+=test_dic
+    return result, test
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
     movie_json = open('data/movie.json', encoding='UTF8')
--- 2회차/이준엽/06.py
+++ 2회차/이준엽/06.py
@@ -5,8 +5,23 @@ from pprint import pprint
 def movie_info(movies, genres):
     pass 
     # 여기에 코드를 작성합니다.  
-        
-        
+    result = []
+    for movie in movies_list :
+        g_name = []
+        g_id = movie.get('genre_ids')
+        for id in g_id :
+            for g_list in genres_list:
+                if g_list['id'] == id :
+                    g_name.append(g_list.get('name'))
+        indimovie = {
+            'genre_name': g_name,
+            'id': movie.get('id'),
+            'overview' : movie.get('overview'),
+            'title' : movie.get('title'),
+            'vote_average' : movie.get('vote_average')
+        }
+        result.append(indimovie)
+    return result
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
     movies_json = open('data/movies.json', encoding='UTF8')
