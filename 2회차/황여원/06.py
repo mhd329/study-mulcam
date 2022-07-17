77979ae44ce4a3680606c1691b198029ba70095e
황여원 <yeooness@naver.com>
2022-07-17 19:59:41 +0900

    2회차 황여원 문제풀이

--- 2회차/황여원/06.py
+++ 2회차/황여원/06.py
@@ -5,22 +5,23 @@ from pprint import pprint
 def movie_info(movies, genres):
     pass 
     # 여기에 코드를 작성합니다. 
-    genre_names = []   
+    result2 = []   
     for movie in movies : 
+        gnames = [] 
         for num in movie.get('genre_ids'):
             for i in genres:
                 if num == i.get('id'):
-                    genre_names.append(i.get('name'))
+                    gnames.append(i.get('name'))
 
-    result = {
-        'id' : movie.get('id'),
-        'title' : movie.get('title'),
-        'vote_average' : movie.get('vote_average'),
-        'overview' : movie.get('overview'),
-        'genre_names' : genre_names
+        result = {
+            'id' : movie.get('id'),
+            'title' : movie.get('title'),
+            'vote_average' : movie.get('vote_average'),
+            'overview' : movie.get('overview'),
+            'genre_names' : gnames
             }
-
-    return result
+        result2.append(result)
+    return result2
 
 
 # 아래의 코드는 수정하지 않습니다.
