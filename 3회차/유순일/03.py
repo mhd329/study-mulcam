4d5684960f0eb61a07d3cdf29dac582cd251bcbe
yoosoonil <y2k7654@gmail.com>
2022-07-18 08:34:54 +0900

    파이썬 프로젝트_01 문제풀이

--- 3회차/유순일/03.py
+++ 3회차/유순일/03.py
@@ -1,15 +1,16 @@
 # 이태극님 코드 참고
+# 딕셔너리 만드는 코드 / 과일이 key이고 숫자가 value.
 with open('./data/fruits.txt', 'r',encoding="utf-8") as f:
     #word = input()
     text=f.read()
     fruit=text.split("\n")
-    fruit_dict = {}
+    fruit_dict = {}                 # 딕셔너리 코드.
 
     for i in fruit:
         if i in fruit_dict:
-            fruit_dict[i] += 1
+            fruit_dict[i] += 1      # 딕셔너리 안에 문자열을 불러오면서 같은것들은 하나씩 더해.
         else:
-            fruit_dict[i] = 1
+            fruit_dict[i] = 1       # 기본값은 1이야.
     with open('03.txt', 'w',encoding="utf-8") as a:
         for k,v in fruit_dict.items():
              a.write(str(k)+" ")
