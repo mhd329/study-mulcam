513ebd6dff38c7e3fe086ace7f233cb39899bd6a
chul1631 <sonky201@gmail.com>
2022-07-17 14:30:52 +0900

    1회차 손세철 폴더 생성3

--- 1회차/손세철/손세철/04.py
+++ 1회차/손세철/손세철/04.py
@@ -1,11 +1,23 @@
-import json
+with open('04.txt', 'w', encoding = 'utf-8') as f:
+    import json
 from pprint import pprint
 
-
 def movie_info(movie):
-    print(movie)
-    
-    # 여기에 코드를 작성합니다.    
+    return {'genre_ids' : movie['genre_ids'],
+            'id' : movie['id'],
+            'overview' : movie['overview'],
+            'title' : movie['title'],
+            'vote_average' : movie['vote_average']}
+
+# 아래의 코드는 수정하지 않습니다.
+if __name__ == '__main__':
+    movie_json = open('data/movie.json', encoding='UTF8')
+    movie = json.load(movie_json)
+
+    pprint(movie_info(movie))
+    a = movie_info(movie)
+for i in a['genre_ids'] :
+    print(i+6) 
 
 
 # 아래의 코드는 수정하지 않습니다.
