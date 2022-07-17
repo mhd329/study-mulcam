423527bd6140a16becc170558a06659a6cb2cbe6
ksy133900 <tkdgns13578@gmail.com>
2022-07-17 16:06:00 +0900

    파이썬 프로젝트

--- 1회차/김상훈/03.py
+++ 1회차/김상훈/03.py
@@ -1,16 +1,5 @@
 with open("data/fruits.txt", "r", encoding="utf-8") as f:
     line = f.readlines()
-<<<<<<< HEAD
-    dic = {}
-    for i in line:
-        i = i[:-1]
-        dic[i] = dic.get(i, 0) + 1
-        
-with open("03.txt", "w", encoding="utf-8") as ff:
-    for key in dic:
-        ff.write(f"{key} {dic[key]}\n") 
-=======
-    print(line)
     dictionary = {}
     for i in line:
         i = i[:-1] #맨뒤 /n 제거
@@ -19,4 +8,3 @@ print(dictionary)
 with open("03.txt", "w", encoding="utf-8") as ff:
     for key in dictionary:
         ff.write(f"{key} {dictionary[key]}\n") 
->>>>>>> 6cb3752de837207a52ee476bb4a4abebc19c0b6f
--- 1회차/김상훈/Q1.py
+++
@@ -1,5 +0,0 @@
-import os
-with open('00.py','w', encoding='utf-8') as f:
-    f.wrtie('N회차 김상훈\n')
-    for i in range(1,6):
-        f.write(f'{i}일차 파이썬 공부 중\n')
