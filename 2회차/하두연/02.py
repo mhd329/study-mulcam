0ad9cae051465e3f1aae7869580716eea4553b41
hady1024 <hady1024@gmail.com>
2022-07-18 01:20:47 +0900

    개인 프로젝트

--- 2회차/하두연/02.py
+++ 2회차/하두연/02.py
@@ -8,7 +8,8 @@ with open('./data/fruits.txt', 'r', encoding='utf-8') as f:
     
     for i in names:
         if i.endswith('berry'):
-            if not i in a: #. not ~ in 텍스트(리스트) 내에 값이 없는지 확인
+            if not i in a: 
+                #. not ~ in 텍스트(리스트) 내에 값이 없는지 확인
                 count += 1
                 a.append(i)
 
