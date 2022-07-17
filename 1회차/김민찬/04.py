14367d4eb77bd234aa312ab992bedd643ec059cc
kmckmc5587 <kmckmc5587@gmail.com>
2022-07-17 09:41:16 +0900

    프로젝트_김민찬

--- 1회차/김민찬/04.py
+++ 1회차/김민찬/04.py
@@ -4,7 +4,12 @@ from pprint import pprint
 
 def movie_info(movie):
     pass 
-    # 여기에 코드를 작성합니다.    
+    # 여기에 코드를 작성합니다.
+    key_list = ['genre_ids', 'id', 'overview', 'title', 'vote_average']
+    movie_info_dict = {}
+    for key in key_list:
+        movie_info_dict[key] = movie[key]
+    return movie_info_dict
 
 
 # 아래의 코드는 수정하지 않습니다.
--- 1회차/김민찬/06.py
+++ 1회차/김민찬/06.py
@@ -5,7 +5,28 @@ from pprint import pprint
 def movie_info(movies, genres):
     pass 
     # 여기에 코드를 작성합니다.  
-        
+    movies_info_dict = []
+
+    for movie in movies:
+        genre_ids = movie['genre_ids']
+
+        gerne_names = []
+
+        for genre in genres: 
+            if genre['id'] in genre_ids:
+                gerne_names.append(genre['name']) 
+
+        key_list = ['id', 'title', 'poster_path', 'vote_average', 'overview']
+
+        movie_info_dict = {}
+
+        for key in key_list:
+            movie_info_dict[key] = movie[key]
+
+        movie_info_dict['gerne_names'] = gerne_names
+
+        movies_info_dict.append(movie_info_dict)
+    return movies_info_dict
         
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
