8eebf898cbccc3da4fddb8f4ec078f3ab50fcfc3
황여원 <yeooness@naver.com>
2022-07-17 19:09:08 +0900

    2반 황여원 문제풀이

--- 2회차/황여원/04.py
+++ 2회차/황여원/04.py
@@ -3,10 +3,16 @@ from pprint import pprint
 
 
 def movie_info(movie):
-    pass 
-    # 여기에 코드를 작성합니다.    
-
-
+    # id, title, vote_average, overview, genre_ids
+    result = {
+        'id' : movie.get('id'),
+        'title' : movie.get('title'),
+        'vote_average' : movie.get('vote_average'),
+        'genre_ids' : movie.get('genre_ids')
+            }
+    print(result)
+    return(result)
+    
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
     movie_json = open('data/movie.json', encoding='UTF8')
--- 2회차/황여원/06.py
+++ 2회차/황여원/06.py
@@ -4,9 +4,25 @@ from pprint import pprint
 
 def movie_info(movies, genres):
     pass 
-    # 여기에 코드를 작성합니다.  
-        
-        
+    # 여기에 코드를 작성합니다. 
+    genre_names = []   
+    for movie in movies : 
+        for num in movie.get('genre_ids'):
+            for i in genres:
+                if num == i.get('id'):
+                    genre_names.append(i.get('name'))
+
+    result = {
+        'id' : movie.get('id'),
+        'title' : movie.get('title'),
+        'vote_average' : movie.get('vote_average'),
+        'overview' : movie.get('overview'),
+        'genre_names' : genre_names
+            }
+
+    return result
+
+
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
     movies_json = open('data/movies.json', encoding='UTF8')
