54388d4c159f534e557e3b59996f0bc7b645e47a
jhkim9028 <jhkim9028@gmail.com>
2022-07-18 08:27:55 +0900

    1기_1회차_김재형

--- 1회차/김재형/06.py
+++ 1회차/김재형/06.py
@@ -4,9 +4,28 @@ from pprint import pprint
 
 def movie_info(movies, genres):
     pass 
-    # 여기에 코드를 작성합니다.  
-        
-        
+    # 여기에 코드를 작성합니다.
+    movie_list = []
+    for mov in movies:
+        def genres_name():
+            genres_name = []
+            for gen_id in mov['genre_ids']:
+                for gen_num in genres:
+                    if gen_id == gen_num.get("id"):
+                        genres_name.append(gen_num.get("name"))
+                        
+            return genres_name
+        result = {
+            'genre_names' : genres_name(),
+            'id' : mov.get('id'),
+            'title' : mov.get('title'),
+            'overview' : mov.get('overview'),
+            'vote_average' : mov.get('vote_average')
+        }
+
+        movie_list.append(result)
+
+    return movie_list
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
     movies_json = open('data/movies.json', encoding='UTF8')
