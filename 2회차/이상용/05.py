42b913685b959d5359732089df25c34201d1e88c
leesangyong94 <tkddydakdhkd@naver.com>
2022-07-18 01:37:18 +0900

    1기 2회차 이상용 과제제출(최종)

--- 2회차/이상용/05.py
+++ 2회차/이상용/05.py
@@ -1,3 +1,6 @@
+# 영화 데이터 movie.json 와 genres.json 을  활용하여 필요한 정보로만 구성된 딕셔너리를 출력하시오. 
+# id, title, vote_average, overview, genre_names로 결과를 만듭니다.
+
 import json
 from pprint import pprint
 
@@ -5,6 +8,19 @@ from pprint import pprint
 def movie_info(movie, genres):
     pass 
     # 여기에 코드를 작성합니다.  
+    gnames = []
+    for i in movie['genre_ids']:
+        for j in genres:
+            if i == j['id']:
+                gnames.append(j['name'])
+
+    result = {'genre_names': gnames,
+            'id': movie.get('id'),
+            'overview': movie.get('overview'),
+            'titile': movie.get('title'),
+            'vote_average': movie.get('vote_average')}
+
+    return result
         
 
 # 아래의 코드는 수정하지 않습니다.
