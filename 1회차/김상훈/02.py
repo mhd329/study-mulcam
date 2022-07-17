6cb3752de837207a52ee476bb4a4abebc19c0b6f
ksy133900 <tkdgns13578@gmail.com>
2022-07-17 15:45:04 +0900

    파이썬 프로젝트

--- 1회차/김상훈/02.py
+++ 1회차/김상훈/02.py
@@ -5,16 +5,16 @@ with open("data/fruits.txt", 'r', encoding='utf-8') as f:
     a=f.readlines()
     print(a)
     cnt=0
-    c=[]
+    b=[]
     a=set(a)
     for i in a:
         k = i[:-1]
         if k.endswith('berry'):
             cnt+=1
-            c.append(i)
+            b.append(i)
 print(cnt)   
 with open("02.txt", 'w', encoding='utf-8') as fw:
     fw.write(f'{cnt}\n')
-    for j in c:
+    for j in b:
         print(j)
         fw.write(j)
--- 1회차/김상훈/03.py
+++ 1회차/김상훈/03.py
@@ -1,10 +1,11 @@
 with open("data/fruits.txt", "r", encoding="utf-8") as f:
     line = f.readlines()
-    dic = {}
+    print(line)
+    dictionary = {}
     for i in line:
-        i = i[:-1]
-        dic[i] = dic.get(i, 0) + 1
-        
+        i = i[:-1] #맨뒤 /n 제거
+        dictionary[i] = dictionary.get(i, 0) + 1
+print(dictionary)
 with open("03.txt", "w", encoding="utf-8") as ff:
-    for key in dic:
-        ff.write(f"{key} {dic[key]}\n") 
+    for key in dictionary:
+        ff.write(f"{key} {dictionary[key]}\n") 
