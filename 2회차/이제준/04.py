ab5c979a07769eb38e304d4125884a2833a6eb28
jejoonlee <jejoonlee1996@gmail.com>
2022-07-17 15:37:37 +0900

    문제풀이 완료

--- 2회차/이제준/04.py
+++ 2회차/이제준/04.py
@@ -3,8 +3,13 @@ from pprint import pprint
 
 
 def movie_info(movie):
-    pass 
-    # 여기에 코드를 작성합니다.    
+    return {                                        # return을 통해 output을 정한다
+        'genre_ids': movie.get('genre_ids'),
+        'id': movie.get('id'),
+        'title': movie.get('title'),
+        'vote_average': movie.get('vote_average'),
+        'overview': movie.get('overview')
+    }
 
 
 # 아래의 코드는 수정하지 않습니다.
--- 2회차/이제준/05.py
+++ 2회차/이제준/05.py
@@ -2,9 +2,23 @@ import json
 from pprint import pprint
 
 
-def movie_info(movie, genres):
-    pass 
-    # 여기에 코드를 작성합니다.  
+def movie_info(movie, genres_list):
+    list = movie.get('genre_ids')       # list라는 변수를 만든다 (genre_ids의 리스트가 들어가 있음)
+    genre_names = []                    
+        # genre_names라는 list를 만든다
+
+    for i in list:                      # 'movie.json' 폴더 genre_ids, list에 있는 숫자들을 꺼낸다 (list to int)
+        for names in genres_list:       # 'genres.json' 폴더에 있는 리스트들을 하나씩 확인한다
+            if names['id'] == i:        # id의 값과 genre_ids 값이 같으면
+                genre_names.append(names['name'])   # genre_names의 리스트에 항목을 추가한다
+
+    return{
+        'id': movie.get('id'),
+        'title': movie.get('title'),
+        'vote_average': movie.get('vote_average'),
+        'overview': movie.get('overview'),
+        'genre_names': genre_names
+    }
         
 
 # 아래의 코드는 수정하지 않습니다.
